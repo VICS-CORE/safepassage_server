91b2bf0d17ccdaca7d69a1ef933e69f5740390cf
Mayank Bhagya <mayankbhagya@gmail.com>
2020-04-20 22:40:42 +0530

    Changing deploy action and script.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "Starting deploy.sh...", $(date)
+echo "==== Starting deploy.sh...", $(date)
 pushd ~/safepassage_server/COVIDSafepassage
 git pull
+echo "==== Installing python dependencies..."
+pip3 install -r requirements.txt
+echo "==== Applying migrations..."
 python3.6 manage.py migrate
+echo "==== Restarting apache..."
 touch COVIDSafepassage/wsgi.py
 popd
-echo "Done"
+echo "==== Done"
