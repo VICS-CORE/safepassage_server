db58ebf3af56c6162553809083a8eefff469a530
Mayank Bhagya <mayankbhagya@gmail.com>
2020-04-21 01:16:16 +0530

    Updating deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 echo "==== Starting deploy.sh...", $(date)
 pushd ~/safepassage_server/COVIDSafepassage
+git stash
 git pull
+git stash pop
 echo "==== Installing python dependencies..."
 pip3 install -r requirements.txt
 echo "==== Applying migrations..."
